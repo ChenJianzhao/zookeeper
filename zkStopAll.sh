463d6e4183f58228dd5d1c01b4d95304d2eb64fa
陈键钊 <cjz@chenjianzhaodeMacBook-Pro.local>
2017-05-23 00:13:04 +0800

    Init git

---
+++ zkStopAll.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+~/Documents/devtool/zookeeper/serverA/zookeeper-3.4.10/bin/zkServer.sh stop
+~/Documents/devtool/zookeeper/serverB/zookeeper-3.4.10/bin/zkServer.sh stop
+~/Documents/devtool/zookeeper/serverC/zookeeper-3.4.10/bin/zkServer.sh stop
